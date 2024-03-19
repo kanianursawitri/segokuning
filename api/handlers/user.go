2f0cca7f498fca622356343bb76c606a943735ff
Kasjful Kurniawan <kasful.bpp@gmail.com>
2024-03-19 11:04:47 +0800

    refactor: remove unused function

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -81,10 +81,6 @@ func (a RegisterRequest) Validate() error {
 	)
 }
 
-func isValidCredType(credType string) bool {
-	return credType == "email" || credType == "phone"
-}
-
 func isValidEmail(email string) bool {
 	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
 	return regexp.MustCompile(emailRegex).MatchString(email)
