4efa5c82c9d604a5a0ddfa8a8a2120ea97133cda
kanianursawitri <kaniamalimba@gmail.com>
2024-03-21 21:29:46 +0700

    add indexing on friends

--- db/migrations/000003_create_friends_table.up.sql
+++ db/migrations/000003_create_friends_table.up.sql
@@ -4,3 +4,9 @@ create table if not exists friends(
     friend_id int not null references users(id),
     created_at timestamptz not null default current_timestamp
 );
+
+-- create index on user_id and friend_id
+create index on friends(user_id);
+create index on friends(friend_id);
+-- create unique index on user_id and friend_id
+create unique index on friends(user_id, friend_id);
